d28bb8b446b86636cc9c17e3b62e43d48512b90d
JON <jonn.thinkrtc@gmail.com>
2017-09-11 17:38:10 -0700

    ruby on rails using webpack for integration with react

---
+++ app/javascript/components/SampleComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SampleComponent extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Sample Component here</h1>
+      </div>
+
+    )
+
+  }
+
+};
+
+
+export default SampleComponent;
